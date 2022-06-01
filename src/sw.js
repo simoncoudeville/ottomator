c904f883a5219d4a2dfe0be967733964e058dd39
Simon <simon.coudeville@howest.be>
2022-06-01 12:07:12 +0200

    Added fetch script

--- src/sw.js
+++ src/sw.js
@@ -13,3 +13,18 @@ async function activate() {
     );
 }
 addEventListener('activate', e => e.waitUntil(activate()));
+
+async function fetch() {
+    const request = event.request;
+    const cache = await caches.open(version);
+    const response = await cache.match(request);
+    if (response) {
+        return response;
+    }
+    const networkResponse = await fetch(request);
+    if (networkResponse) {
+        await cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+}
+addEventListener('fetch', e => e.respondWith(fetch()));
